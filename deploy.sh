c5a10a0e7ad8ea7d0220e647e144d873769a6f2b
discipled <disciple.ding@gmail.com>
2016-10-12 11:55:06 +0800

    refact(travis): Refactor travis git operator.

--- deploy.sh
+++ deploy.sh
@@ -1,40 +1,39 @@
 #!/bin/bash
 set -e # Exit with nonzero exit code if anything fails
 
-SOURCE_BRANCH="real-world"
+# Git variables
+TARGET_FOLDER="build"
 TARGET_BRANCH="gh-pages"
+
+# Travis encrypt variables
+ENCRYPTED_KEY="encrypted_${ENCRYPTION}_key"
+ENCRYPTED_IV="encrypted_${ENCRYPTION}_iv"
 
 # Save some useful information
 REPO=`git config remote.origin.url`
 SSH_REPO=${REPO/https:\/\/github.com\//git@github.com:}
 SHA=`git rev-parse --verify HEAD`
 
-# Clone the existing gh-pages for this repo into tmp
-# Create a new empty branch if gh-pages doesn't exist yet (should only happen on first deploy)
-git clone $REPO tmp
-cd tmp
-git checkout $TARGET_BRANCH || git checkout --orphan $TARGET_BRANCH
-cd ..
-
-# Clean out existing contents
-rm -rf tmp/**/* || exit 0
-
 # Build source
 npm run build
-cp -r build/ tmp/
 
-# Set committer git info.
-cd tmp
+# Set committer git info
 git config user.name "Disciple_D"
 git config user.email "disciple.ding@gmail.com"
 
-# If there are no changes to the compiled out (e.g. this is a README update) then just bail.
-if [ -z `git diff --exit-code` ]; then
-    echo "No changes to the output on this push; exiting."
-    exit 0
-fi
+# Force add build folder to git
+git add -f $TARGET_FOLDER
 
-# Commit changes & Push commit to TARGET_BRANCH.
-git add .
+# Commit the build code, that is a local commit for git subtree split
 git commit -m "Deploy to GitHub Pages: ${SHA}"
-git push $SSH_REPO $TARGET_BRANCH
+
+# Split build file as a $TARGET_BRANCH of git
+git subtree split -P $TARGET_FOLDER -b $TARGET_BRANCH
+
+# Add ssh authorization
+openssl aes-256-cbc -K ${!ENCRYPTED_KEY} -iv ${!ENCRYPTED_IV} -in deploy_key.enc -out deploy_key -d
+eval `ssh-agent -s`
+ssh-add deploy_key
+
+# Push code to git
+git push -f $SSH_REPO $TARGET_BRANCH
